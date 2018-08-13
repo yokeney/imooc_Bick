0c9ec346cd8a28cca837d9fdceaf28f801c65d14
yokeney <965660471@qq.com>
2018-08-13 23:27:34 +0800

    componment

--- src/admin.js
+++ src/admin.js
@@ -2,6 +2,8 @@ import React,{Component} from "react";
 import {Row,Col} from 'antd'
 import Header from './compoments/Header'
 import Footer from './compoments/Footer'
+import NavList from './compoments/NavList'
+import './style/common.less'
  export default class Admin extends Component{
  constructor(){
    super();
@@ -9,14 +11,15 @@ import Footer from './compoments/Footer'
    }
    render(){
      return (
-          <Row>
-          <Col span="3">
-              lwft
+          <Row className="container">
+          <Col span="3" className="main">
+              <NavList />
             </Col>
           <Col span="21">
               <Header>Header</Header>
-                <Row>
-                  content
+                <Row className="content">
+                content
+                  {/*{this.props.children}*/}
                 </Row>
               <Footer>Footer</Footer>
             </Col>
--- src/compoments/NavList/index.js
+++ src/compoments/NavList/index.js
@@ -1,4 +1,9 @@
 import React,{Component} from "react";
+import menuList from '../../config/menuConfig'
+import {Menu,Icon} from 'antd'
+import './index.less'
+const SubMenu=Menu.SubMenu
+const MenuItemGroup=Menu.ItemGroup
  export default class NavList extends Component{
  constructor(){
    super();
@@ -6,7 +11,22 @@ import React,{Component} from "react";
    }
    render(){
      return (
-            <div>NavList</div>
+            <div>
+              <div className="logo">
+                <img src="/assets/logo-ant.svg" alt=""/>
+                <h1>Immoc MS</h1>
+              </div>
+              <Menu theme="dark">
+                <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+                  <MenuItemGroup title="Item 1">
+                    <Menu.Item key="1">Option 1</Menu.Item>
+                    <Menu.Item key="2">Option 2</Menu.Item>
+                    <Menu.Item key="3">Option 3</Menu.Item>
+                    <Menu.Item key="4">Option 4</Menu.Item>
+                  </MenuItemGroup>
+                </SubMenu>
+              </Menu>
+            </div>
      )
    }
  }
