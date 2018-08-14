845c79e45cb8a3984a532bb7b99160ee493dc1f2
yokeney <965660471@qq.com>
2018-08-14 22:44:14 +0800

    componment2

--- src/compoments/NavList/index.js
+++ src/compoments/NavList/index.js
@@ -9,6 +9,26 @@ const MenuItemGroup=Menu.ItemGroup
    super();
 
    }
+   componentWillMount(){
+     const  meneuTreeNode=this.renderMenu(menuList);
+     this.setState({
+       meneuTreeNode
+     })
+
+
+   }
+   renderMenu=(data)=>{
+        return data.map((item)=>{
+          if (item.children) {
+            return (
+              <SubMenu title={item.title} key={item.key}>
+              {this.renderMenu(item.children)}
+              </SubMenu>
+            )
+          }
+          return <Menu.Item  title={item.title} key={item.key}>{item.title}</Menu.Item>
+        })
+   }
    render(){
      return (
             <div>
@@ -17,14 +37,7 @@ const MenuItemGroup=Menu.ItemGroup
                 <h1>Immoc MS</h1>
               </div>
               <Menu theme="dark">
-                <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                  <MenuItemGroup title="Item 1">
-                    <Menu.Item key="1">Option 1</Menu.Item>
-                    <Menu.Item key="2">Option 2</Menu.Item>
-                    <Menu.Item key="3">Option 3</Menu.Item>
-                    <Menu.Item key="4">Option 4</Menu.Item>
-                  </MenuItemGroup>
-                </SubMenu>
+                {this.state.meneuTreeNode}
               </Menu>
             </div>
      )
