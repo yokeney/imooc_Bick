4c18bdd582532bf976b8ea771a45962db54d8178
yokeney <965660471@qq.com>
2018-11-05 15:46:40 +0800

    11 is complite

--- src/page/user/index.js
+++ src/page/user/index.js
@@ -3,6 +3,7 @@
  import util from "../../util/util"
  import Baseform from "../../compoments/baseForm"
  import axios from "./../../axios/index"
+ import moment from 'moment'
  import Etable from "../../compoments/Etable"
  const FormItem=Form.Item;
  const RadioGroup=Radio.Group;
@@ -13,7 +14,7 @@
          page:1
      }
      state={
-         isVisible:false
+         isVisible:false,
      }
      formList=[
          {
@@ -50,6 +51,7 @@
          axios.requestList(this,'/tableList',this.params)
      }
      openOrderDetail=(type)=>{
+         let item=this.state.selectItem;
          console.log(type);
          if (type=="create") {
              this.setState({
@@ -58,15 +60,66 @@
                  title:"创建员工"
              })
          }
-         else if(type==="edit"){
-             
+         else if(type=="edit"){
+             if (!item) {
+                 Modal.info({
+                     title:"提示",
+                     content:'请选择一个用户'
+                 })
+                 return
+             }
+             console.log(item);
+             this.setState({
+                 type,
+                 isVisible:true,
+                 title:"编辑员工",
+                 userInfo:item
+             })
+         }
+         else if(type=='detail'){
+             this.setState({
+                 type,
+                 isVisible:true,
+                 title:"员工详情",
+                 userInfo:item
+             })
+         }
+         else if (type=="delete") {
+             let _this=this;
+                if (!item) {
+                    Modal.info({
+                        title:"提示",
+                        content:'请选择一个用户'
+                    })
+                    return
+                }
+                Modal.confirm({
+                    title:"确人删除？",
+                    onOk(){
+                        axios.ajax({
+                            url:"/userdelete",
+                            data:{
+                                params:{
+                                    id:item.id
+                                }
+                            }
+                        }).then((res)=>{
+                            if (res.code=='0') {
+                                _this.setState({
+                                    isVisible:false
+                                })
+                                _this.request();
+                            }
+                        })
+                    }
+                })
          }
       }
       handleSubmit=()=>{
           let type=this.state.type;
           let data=this.userForm.props.form.getFieldsValue();
           axios.ajax({
-              url:"/userAdd",
+              url:type=='create'?"/userAdd":"useredit",
               data:{
                   params:data
               }
@@ -136,10 +189,16 @@
              dataIndex:'time'
              }
      ]
+     let footer={};
+     if (this.state.type=='detail') {
+         footer={
+             footer:null
+         }
+     }
           return (
             <div>
                 <Card>
-                    <Baseform formList={this.formList} filterSubmit={this.handleFilter}  />
+                    <Baseform formList={this.formList}  filterSubmit={this.handleFilter}  />
                 </Card>
                 <Card style={{marginTop:10}}>
                     <Button type="primary" icon="plus" onClick={()=>this.openOrderDetail('create')}>添加员工</Button>
@@ -152,7 +211,7 @@
                      updateSelectedItem={util.updateSelectedItem.bind(this)}
                      columns={column}
                      dataSource={this.state.list}
-                     rowSelection="checkbox"
+                     rowSelection="radio"
                      selectedIds={this.state.selectedIds}
                      selectItem={this.state.selectItem}
                      selectedRowKeys={this.state.selectedRowKeys}
@@ -168,15 +227,27 @@
                     })
                 }}
                 width={600}
+                {...footer}
                 >
-                <UserForm type={this.state.type} wrappedComponentRef={(inst)=>this.userForm=inst}></UserForm>
+                <UserForm type={this.state.type}  userInfo={this.state.userInfo} wrappedComponentRef={(inst)=>this.userForm=inst}></UserForm>
                 </Modal>
              </div>
          )
      }
  }
 class UserForm extends Component{
+    getState=(status)=>{
+            return{
+                '1':'咸鱼一条',
+                '2':'凤凰鸣',
+                '3':'无衣',
+                '4':'六六',
+            }[status]
+        }
     render(){
+        let type=this.props.type;
+        let userInfo=this.props.userInfo ||{};
+        console.log(userInfo);
         const {getFieldDecorator} =this.props.form;
         const formitemLayout={
             labelCol:{span:5},
@@ -186,14 +257,20 @@ class UserForm extends Component{
             <Form layout="horizontal">
                 <FormItem label="用户名" {...formitemLayout}>
                 {
-                    getFieldDecorator("user_name")(
+                    type=="detail"?userInfo.userName:
+                    getFieldDecorator("user_name",{
+                        initialValue:userInfo.userName
+                    })(
                         <Input type="text" placeholder="请输入用户名" />
                     )
                 }
                 </FormItem>
                 <FormItem label="性别" {...formitemLayout}>
                 {
-                    getFieldDecorator("sex")(
+                    type=="detail"?(userInfo.sex=="1"?'男':'女'):
+                    getFieldDecorator("sex",{
+                        initialValue:userInfo.sex
+                    })(
                         <RadioGroup>
                             <Radio value={1}>
                                 男
@@ -207,7 +284,10 @@ class UserForm extends Component{
                 </FormItem>
                 <FormItem label="状态" {...formitemLayout}>
                 {
-                    getFieldDecorator("status")(
+                    type=="detail"?this.getState(userInfo.status):
+                    getFieldDecorator("status",{
+                        initialValue:userInfo.status
+                    })(
                         <Select>
                         <Option value={1}>1</Option>
                         <Option value={2}>2</Option>
@@ -220,14 +300,22 @@ class UserForm extends Component{
                 </FormItem>
                 <FormItem label="生日" {...formitemLayout}>
                 {
-                    getFieldDecorator("birthday")(
+                    type=="detail"?userInfo.status:
+                    getFieldDecorator("birthday",{
+                        initialValue:moment(userInfo.status)
+
+                    })(
                         <DatePicker />
                     )
                 }
+
                 </FormItem>
                 <FormItem label="联系地址" {...formitemLayout}>
                 {
-                    getFieldDecorator("address")(
+                    type=="detail"?userInfo.address:
+                    getFieldDecorator("address",{
+                        initialValue:userInfo.address
+                    })(
                         <TextArea row={3} placeholder="请输入联系地址"/>
                     )
                 }
